9e187ad3ab1db5f31b8543a2d38b7bef6a5c0a80
seungkey <61777003+seungkey@users.noreply.github.com>
2021-05-25 23:02:42 +0900

    Create (level1)신규 아이디 추천.py

---
+++ (level1)신규 아이디 추천.py
@@ -0,0 +1,32 @@
+import re
+def solution(new_id):
+    ni=new_id.lower()
+    a=re.compile('[a-z0-9-_.]')
+    ni=''.join(a.findall(ni))
+    c=ni.count('..')
+    while c:
+        c=ni.count('..')
+        if '..' in ni:
+            ni=ni.replace('..','.')
+        else:
+            break
+    ni=list(ni)
+    
+    if ni[0]=='.':
+        del ni[0]
+    if ni=='':
+        pass
+    else:
+        if ni[-1]=='.':
+            del ni[-1]
+    if len(ni)==0:
+        ni.append('a')
+    if len(ni)>=16:
+        ni=ni[:15]
+        if ni[-1]=='.':
+            del ni[-1]
+    while len(ni)<=2:
+        ni.append(ni[-1])
+    print(ni)
+    answer = ''.join(ni)
+    return answer
