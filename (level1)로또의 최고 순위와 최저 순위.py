9dfdf05eadf871c30b5ae45471f60fbdcd8360e9
seungkey <84698380+firstep710@users.noreply.github.com>
2021-05-26 00:25:28 +0900

    Create (level1)로또의 최고 순위와 최저 순위.py

---
+++ (level1)로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,16 @@
+def solution(lottos, win_nums):
+    answer = []
+    win_nums=set(win_nums)
+    a=[]
+    correct=0
+    for i in lottos:
+        if i in win_nums:
+            a.append(i)
+        elif i==0:
+            correct+=1
+    correct+=len(a)
+    rank={0:6,1:6,2:5,3:4,4:3,5:2,6:1}
+    answer.append(rank[correct])
+    answer.append(rank[len(a)])
+
+    return answer
